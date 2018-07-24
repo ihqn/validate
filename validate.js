642f76fa18cf96006ba0336cdfef2d3d8567140d
tianqi <ihqn19@gmail.com>
2018-07-24 15:06:38 +0800

    Revert "添加注释"
    This reverts commit c909deab6fa75e64d9dc40eae0b8e17659308931.

--- validate.js
+++ validate.js
@@ -8,45 +8,39 @@
     else
         root["validate"] = factory();
 })(this, function() {
-    /*
-     *   inputarray，$对象，所要进行判断的元素
-     *   errcall,错误回调 方法
-     *   ifround，是否全部错误都显示出来还是出现第一个错误的时候打断
-     *   newren, 自定义规则 ，可传正则或函数
-    */
     return function(inputarray, errcall, ifround , newren){
         var c = {
-            ren: {
-                cellphone: '^(((1[1-9][0-9]{1})|(17[0-9]{1}))+\\d{8})$',    //手机
-                num: '^\\d*$',
-                decimal: '^\\d*(?:\\.\\d{1,2})?$',//小数
-                phone: '^(((1[1-9][0-9]{1})+\\d{8})|((0\\d{2,3}-?)?[1-9]\\d{6,7})$', //手机或者固话
-                chinese: '^[\\u4E00-\\u9FA5\\uF900-\\uFA2D][\\u4E00-\\u9FA5\\uF900-\\uFA2D1-9a-zA-Z]+$',
-                otempty: function(value) {
+            ren : {
+                cellphone:"^(((13[0-9]{1})|(18[0-9]{1})|(15[0-9]{1}))+\\d{8})$",    //手机
+                num : "^\\d*$",
+                number : "^\\d*(?:\\.\\d{1,2})?$",
+                phone:"^(((13[0-9]{1})|(18[0-9]{1})|(15[0-9]{1}))+\\d{8})|((0\\d{2,3}-?)?[1-9]\\d{6,7})$", //手机或者固话
+                name:"^[\\u4E00-\\u9FA5\\uF900-\\uFA2Da-zA-Z]([\\s.]?[\\u4E00-\\u9FA5\\uF900-\\uFA2Da-zA-Z]+){1,4}$",
+                otempty:function(value){//非空
                     return value.length > 0;
-                }                       //非空
+                },
+                email:/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i, //邮件
+                url:"^http[s]?:\\/\\/([\\w-]+\\.)+[\\w-]+([\\w-./?%&=]*)?$",   //url
+                passwrd:"^[^1-9][\\w-@#$%^&*]{6,20}$"         //密码保证6-20位的英文字母/数字/下划线/减号和@#$%^&*这些符号
             },
-            showErr: errcall,
-            cheakinput: function(input){
-                var that = this,
-                    vails = input.attr('vail'),
-                    errs = input.attr('errormsg');
+            showErr : errcall,
+            cheakinput : function(input){
+                var that = this;
+                var vails = input.attr('vail'),
+                     errs = input.attr('errormsg');
 
-                if (!vails) return true;
-
-                //节点上不含otempty则可以为空
+                //可以为空
                 var ifcareempty = /otempty/gi.test(vails);
                 if( !ifcareempty && input.val() <= 0){
                     return true;
                 }
                 var regs = vails.split('|'),
-                     err = errs.split('|'),
-                     t = true;
+                     err = errs.split('|');
 
-                for (var j = 0, len = regs.length; j < len; j++) {
-                    var icom = regs[j],
-                        ative = (typeof that.ren[icom] == 'function') ? that.ren[icom](input.val(), input) : new RegExp(that.ren[icom]).test(input.val());
-
+                var t = true;
+                for(var j=0, len=regs.length; j<len;j++){
+                    var icom = regs[j];
+                    var ative = (typeof that.ren[icom] == 'function') ? that.ren[icom](input.val(), input) : new RegExp(that.ren[icom]).test(input.val());
                     if(!ative){
                         that.showErr(err[j], input);
                         t = ative;
@@ -59,47 +53,33 @@
                 return t;
             }
         }
-
-        $.extend(c.ren, newren)
-
         return function(){
-            var isSucceed = true;
+            var tt = true;
 
+            jQuery.extend(c.ren, newren)
             inputarray.each(function() {
-                var t = c.cheakinput($(this));
-
-                if (!t) {
-                    isSucceed = t;
-                    if (!ifround) return t;
+                var t =  c.cheakinput($(this));
+                if(!ifround && !t){
+                    tt = t;
+                    return t;
+                }
+                if(!t){
+                    tt = t;
                 }
             });
-            return isSucceed;
+            return tt;
         }
     }
 })
-            
-
-/*
-* 表单验证 依赖jQuery @tianqi
-
-目前含有常用正则：
-cellphone判断手机号
-num 纯数字
-decimal小数
-chinese中文
-otempty 为空返回false
-
-
-使用方法
-*html
-* <input type="tel" vail="otempty|cellphone" errormsg="联系手机不能为空|联系手机不正确哦"  placeholder="请输入手机号码" />
-* 
-*调用
-* 为true 则是验证通过 = validate(需要认证的节点，错误回调，是否全部验证完再返回错误（false为出现一个错误后马上中断返回错误），新添加的正则或者函数)
-*
-* 如：
-$('form').submit(function(){
-    var iftrue  = validate($('form input'), function(that, err){console.log(err, that)}, false, {num: "^\\d*$", ifNum: function(val){return /\d/.test(val)}})();
-    return false
-})
+/*接收参数
+    *inputarray，$对象，所要进行判断的元素
+    *errcall,错误回调 方法
+    *ifround,是否全部错误都显示出来还是出现第一个错误的时候打断
+    *newren,需要新添加的正则
+/*使用方法
+    *html
+    * <input type="tel" vail="otempty|cellphone" errormsg="联系手机不能为空|联系手机不正确哦"  placeholder="请输入手机号码" />
+    *调用
+    *var iftrue  = validate($('.js-validate input'), global.errcallbak, false)();
+    *返回true为表单验证通过
 **/
